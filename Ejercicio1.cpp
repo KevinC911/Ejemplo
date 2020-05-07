fd121b9f37682d455758ce11f1e0dee64bb06935
KevinC911 <00157320@uca.edu.sv>
2020-05-06 19:45:01 -0600

    Ejercicio1

---
+++ Ejercicio1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    int numero;
+
+    cout<<"Ingresa un numero porfavor";
+    cin>>numero;
+
+    cout<<numero;
+
+    return 0;
+
+}
